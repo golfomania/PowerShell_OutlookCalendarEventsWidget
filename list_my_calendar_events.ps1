a32b7f7dae7c106cb6b6fd60b91a67af3341d962
Martin Loeffler <35120458+golfomania@users.noreply.github.com>
2023-11-08 07:39:37 +0100

    initial upload

---
+++ list_my_calendar_events.ps1
@@ -0,0 +1,35 @@
+#############################################
+# List my next calender events from lokal Outlook (no MS Graph API needed)
+# Martin Löffler 
+# 02.11.2023
+# WORKING (but without regular meetings)
+#############################################
+
+Function Get-OutlookCalendar  
+ {  
+  # use Outlook interop to get the default calendar folder
+  Add-Type -Path "C:\Program Files\Microsoft Office\root\Office16\ADDINS\Microsoft Power Query for Excel Integrated\bin\Microsoft.Office.Interop.Outlook.dll" | out-null
+  $olFolders = “Microsoft.Office.Interop.Outlook.OlDefaultFolders” -as [type]  
+  $outlook = new-object -comobject outlook.application  
+  $namespace = $outlook.GetNameSpace(“MAPI”)  
+  $folder = $namespace.getDefaultFolder($olFolders::olFolderCalendar) 
+  
+  # get current time and tomorrow to filter the calendar items 
+  $now = Get-Date
+  $tomorrow = (Get-Date).AddDays(1).Date
+
+  # get all calendar items for today which are not ended ye, sort them by start time and select the properties we want to display
+  $folder.items | Where-Object { ($_.End -gt $now) -and ($_.Start -lt $tomorrow) } | Sort-Object -Property Start | Select-Object -Property Subject, Start, Duration, Location | Format-Table -AutoSize 
+
+ } #end function Get-OutlookCalendar  
+ 
+ # call function
+ Clear-Host
+ Get-OutlookCalendar
+
+ # refresh every 10 minutes
+ while ($true) {
+  Clear-Host
+  Get-OutlookCalendar
+  Start-Sleep -Seconds 600
+}
