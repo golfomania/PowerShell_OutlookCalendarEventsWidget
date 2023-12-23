4a91276e9bc86dd046a196ee35a9163598ad598b
Martin Loeffler <ml.loeffler@gmx.de>
2023-12-23 10:19:40 +0100

    code cleanup

--- SQLite_list_my_calendar_events_backend.ps1
+++ SQLite_list_my_calendar_events_backend.ps1
@@ -9,24 +9,26 @@
 # preperation of SQLite DB	
 # download SQLite from https://www.sqlite.org/download.html
 # 
-# create a new DB named calendar.db > .open calendar.db
+# create a new DB named calendar.db with the command: .open calendar.db
 # create a new table named table_meetings with the following columns:
-# id (INTEGER PRIMARY KEY AUTOINCREMENT), subject (TEXT), start (TEXT), duration (TEXT), , startin (TEXT), endin (TEXT), location (TEXT) >
+# id (INTEGER PRIMARY KEY AUTOINCREMENT), subject (TEXT), start (TEXT), duration (TEXT), , startin (TEXT), endin (TEXT), location (TEXT) with the command: 
 # CREATE TABLE meetings_table (id INTEGER PRIMARY KEY, name TEXT, start TEXT, duration INTEGER, endin INTERGER, startin INTEGER, location TEXT);
 #
-# get path to database .databases > e.g. C:\sqlite3\calendar.db
-# check if the table is created .tables
-# check the schama of the table .schema
+# get path to database with the command: .databases  e.g. C:\sqlite3\calendar.db
+# check if the table is created with the command: .tables
+# check the schama of the table with the command: .schema
+# exit the SQLite CLI with the command: .exit
 # 
 # read: SELECT * FROM meetings_table;
 # write: INSERT INTO meetings_table (name, start, duration, endin, startin, location) VALUES ('name1', 'start1', '60', '10', '1', 'location1');
 # delete: DELETE FROM meetings_table WHERE id = 1;
-# .exit
 #############################################
 
 Function Get-OutlookCalendar  
  {  
+  #############################################
   # use Outlook interop to get the default calendar folder
+  #############################################
   # Search your PC in file explorer for "Microsoft.Office.Interop.Outlook.dll" to find one of this DLLs on your machine and paste the path here
   Add-Type -Path "C:\Program Files\Microsoft Office\root\Office16\ADDINS\Microsoft Power Query for Excel Integrated\bin\Microsoft.Office.Interop.Outlook.dll" | out-null
   $olFolders = “Microsoft.Office.Interop.Outlook.OlDefaultFolders” -as [type]  
@@ -54,63 +56,72 @@ Function Get-OutlookCalendar
    Location | Format-Table -AutoSize
 
   
+  #############################################
+  # SQLite DB
+  #############################################
+  # Load the System.Data.SQLite assembly replace with path to your DLL
+  # source https://system.data.sqlite.org/
+  [Reflection.Assembly]::LoadFile("C:\sqlite3\sqlite-netFx46-static-binary-bundle-x64-2015-1.0.118.0\System.Data.SQLite.dll")
 
-   # SQLite DB
-   # Load the System.Data.SQLite assembly replace with path to your DLL
-   # source https://system.data.sqlite.org/
-    [Reflection.Assembly]::LoadFile("C:\sqlite3\sqlite-netFx46-static-binary-bundle-x64-2015-1.0.118.0\System.Data.SQLite.dll")
+  # Create a connection to the database
+  $connection = New-Object System.Data.SQLite.SQLiteConnection
+  # replace with your path to the DB
+  $connection.ConnectionString = "Data Source='C:\sqlite3\calendar.db';Version=3;"
+  $connection.Open()
 
-    # Create a connection to the database
-    $connection = New-Object System.Data.SQLite.SQLiteConnection
-    # replace with your path to the DB
-    $connection.ConnectionString = "Data Source='C:\sqlite3\calendar.db';Version=3;"
-    $connection.Open()
+  # Create a command to insert data into a table
+  # $command = $connection.CreateCommand()
+  # $command.CommandText = "INSERT INTO meetings_table (name, start, duration, endin, startin, location) VALUES ('name11', 'start1', '60', '10', '1', 'location11')"
 
-    # Create a command to insert data into a table
-    # $command = $connection.CreateCommand()
-    # $command.CommandText = "INSERT INTO meetings_table (name, start, duration, endin, startin, location) VALUES ('name11', 'start1', '60', '10', '1', 'location11')"
+  
+  
+  #############################################
+  # add new events to the DB, updated existing events
+  #############################################
+  # get all events from the DB
+  $command = $connection.CreateCommand()
+  $command.CommandText = "SELECT * FROM meetings_table;"
+  $eventFromDatabase = $command.ExecuteReader()
 
-    # Create a command to read data from a table
-    $command = $connection.CreateCommand()
-    $command.CommandText = "SELECT * FROM meetings_table;"
-    $result = $command.ExecuteReader()
-    
-    # Read and display the data
-    while ($result.Read())
-    {
-        Write-Host $result.GetValue(0) $result.GetValue(1) $result.GetValue(2) $result.GetValue(3) $result.GetValue(4) $result.GetValue(5) $result.GetValue(6)
+  # Iterate through these events of the Database and check them against the Outlook events
+  while ($eventFromDatabase.Read())
+  {
+    Write-Host $eventFromDatabase.GetValue(0) $eventFromDatabase.GetValue(1) $eventFromDatabase.GetValue(2) $eventFromDatabase.GetValue(3) $eventFromDatabase.GetValue(4) $eventFromDatabase.GetValue(5) $eventFromDatabase.GetValue(6)
 
-        # check if there is an event in $events where the subject and the Start is the same as in the DB
-        # if not, delete the event from the DB
-        $event = $events | Where-Object {$_.Subject -eq $result.GetValue(1) -and $_.Start.ToString("HH:mm") -eq $result.GetValue(2)}
-        if (!$event) {
-            $command = $connection.CreateCommand()
-            $command.CommandText = "DELETE FROM meetings_table WHERE id = $($result.GetValue(0));"
-            $command.ExecuteNonQuery()
+    # check if there is an event in $events where the subject and the Start is the same as in the DB
+    # if not, delete the event from the DB
+    $event = $events | Where-Object {$_.Subject -eq $eventFromDatabase.GetValue(1) -and $_.Start.ToString("HH:mm") -eq $eventFromDatabase.GetValue(2)}
+    if (!$event) {
+      $command = $connection.CreateCommand()
+      $command.CommandText = "DELETE FROM meetings_table WHERE id = $($eventFromDatabase.GetValue(0));"
+      $command.ExecuteNonQuery()
         
-      }
+    }
       
-      # check if there is an event in $events which is not in the DB
-      # if yes, add it to the DB
-      $event = $events | Where-Object {$_.Subject -eq $result.GetValue(1) -and $_.Start.ToString("HH:mm") -eq $result.GetValue(2)}
-      if (!$event) {
-        $command = $connection.CreateCommand()
-        $command.CommandText = "INSERT INTO meetings_table (name, start, duration, endin, startin, location) VALUES ('$($event.Subject)', '$($event.Start.ToString("HH:mm"))', '$($event.Duration)', '$($event.EndIn)', '$($event.StartIn)', '$($event.Location)')"
-        $command.ExecuteNonQuery()
-      }
+    # check if there is an event in $events which is not in the DB
+    # if yes, add it to the DB
+    $event = $events | Where-Object {$_.Subject -eq $eventFromDatabase.GetValue(1) -and $_.Start.ToString("HH:mm") -eq $eventFromDatabase.GetValue(2)}
+    if (!$event) {
+      $command = $connection.CreateCommand()
+      $command.CommandText = "INSERT INTO meetings_table (name, start, duration, endin, startin, location) VALUES ('$($event.Subject)', '$($event.Start.ToString("HH:mm"))', '$($event.Duration)', '$($event.EndIn)', '$($event.StartIn)', '$($event.Location)')"
+      $command.ExecuteNonQuery()
+    }
         
         
         
      
         
-    }
+  } # end of adding and updating loop 
+
+  #############################################
+  # delete events which are no longer in Outlook from the DB 
+  #############################################
 
     
-    # Execute the command
-    # $command.ExecuteNonQuery()
 
-    # Close the connection
-    $connection.Close()
+
+  # Close the DB connection
+  $connection.Close()
  } #end function Get-OutlookCalendar  
  
 
